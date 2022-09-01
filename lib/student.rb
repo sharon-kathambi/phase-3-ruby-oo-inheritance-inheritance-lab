a10bf57124f6328ec2cb01a09e5ea7668efa7b98
Sharon Mwenda <smkathambi@gmail.com>
2022-09-01 17:43:19 +0300

    commit tree 703ca732c9a6b906cac58e43524c1de5cd7aed25

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user"
 
+class Student < User
+
+   attr_accessor :knowledge
+
+    def initiliaze
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
